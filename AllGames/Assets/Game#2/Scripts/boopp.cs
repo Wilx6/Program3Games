a040f8544272af2581dfd0f9453be7106c450a61
Wilx6 <144147441+Wilx6@users.noreply.github.com>
2024-09-16 16:54:39 -0400

    Again

--- AllGames/Assets/Game#2/Scripts/boopp.cs
+++ AllGames/Assets/Game#2/Scripts/boopp.cs
@@ -14,7 +14,7 @@ public class boopp : MonoBehaviour
 
         if (Input.GetKeyDown("r"))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
     }
 
