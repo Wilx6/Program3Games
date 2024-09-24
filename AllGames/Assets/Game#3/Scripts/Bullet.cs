cb38383440969f3804508e89eae217a210439817
Wilx6 <144147441+Wilx6@users.noreply.github.com>
2024-09-24 05:02:43 -0400

    finished roblox game
    it sucks

---
+++ AllGames/Assets/Game#3/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
